9c6c71ad0200e9e47c8d0c411c3f7a62840b2533
vandunxg <nguyenvandung.official@gmail.com>
2025-11-14 08:31:00 +0700

    init project

---
+++ src/main/java/com/vandunxg/trackee/TrackeeApplication.java
@@ -0,0 +1,13 @@
+package com.vandunxg.trackee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vandunxg/trackee/TrackeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vandunxg.trackee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
