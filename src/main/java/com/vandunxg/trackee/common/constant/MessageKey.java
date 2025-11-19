2de5484cf486c2ad7cfc513e49a64eb263ff405d
vandunxg <nguyenvandung.official@gmail.com>
2025-11-19 23:41:59 +0700

    remove redundant blank lines from MessageKey constants

--- src/main/java/com/vandunxg/trackee/common/constant/MessageKey.java
+++ src/main/java/com/vandunxg/trackee/common/constant/MessageKey.java
@@ -4,7 +4,6 @@ public class MessageKey {
 
     /** COMMON CRUD * */
     public static final String CREATED_SUCCESS = "common.created_success";
-
     public static final String UPDATED_SUCCESS = "common.updated_success";
     public static final String DELETED_SUCCESS = "common.deleted_success";
     public static final String NOT_FOUND = "common.not_found";
@@ -12,7 +11,6 @@ public class MessageKey {
 
     /** AUTH * */
     public static final String AUTH_LOGIN_SUCCESS = "auth.login_success";
-
     public static final String AUTH_LOGIN_FAIL = "auth.login_fail";
     public static final String AUTH_REGISTER_SUCCESS = "auth.register_success";
     public static final String AUTH_TOKEN_INVALID = "auth.token_invalid";
@@ -20,7 +18,6 @@ public class MessageKey {
 
     /** USER * */
     public static final String USER_NOT_FOUND = "user.not_found";
-
     public static final String USER_CREATED = "user.created";
     public static final String USER_UPDATED = "user.updated";
     public static final String USER_DELETED = "user.deleted";
