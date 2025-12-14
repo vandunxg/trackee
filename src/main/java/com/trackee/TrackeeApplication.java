3d3f6d090fb35af6ee7389bf8b3c7c5d41f99e3e
vandunxg <nguyenvandung.official@gmail.com>
2025-12-14 21:10:52 +0700

    refactor: format code and adjust main method accessibility

--- src/main/java/com/trackee/TrackeeApplication.java
+++ src/main/java/com/trackee/TrackeeApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TrackeeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TrackeeApplication.class, args);
-	}
-
+    static void main(String[] args) {
+        SpringApplication.run(TrackeeApplication.class, args);
+    }
 }
